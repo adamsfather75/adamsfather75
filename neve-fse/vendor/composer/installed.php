0284a3bd08fcb275d7e376cc356b7179c7ac8199
Stano Dzavoronok <stano.dzavoronok@xfive.co>
2025-05-29 19:05:30 +0200

    Theme files
    Files for research

---
+++ neve-fse/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve-fse',
+        'pretty_version' => 'v1.1.4',
+        'version' => '1.1.4.0',
+        'reference' => 'c78856021e1b725d4ec25afb9764bbf4d144fb38',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve-fse' => array(
+            'pretty_version' => 'v1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => 'c78856021e1b725d4ec25afb9764bbf4d144fb38',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.46',
+            'version' => '3.3.46.0',
+            'reference' => 'af2e7360bbe3af6a1bd2c3779fc33b7765104461',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
